e387240e6326d5543ca9686c7d6a04b31f960304
larissaashley <larissaashleyy@hotmail.com>
2018-11-02 21:10:06 -0300

    Reestruturação das classes

---
+++ API/DAO.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request, json
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'drbarato'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
+def getMedicos(cidade,especialidade,estado):
+	cur = mysql.connect().cursor()
+	cur.execute('SELECT * FROM MEDICO LEFT JOIN LUGAR ON MEDICO.ID_USUARIO = LUGAR.ID_USUARIO WHERE LUGAR.CIDADE = "'+cidade+'" AND LUGAR.ESTADO = "'+estado+'"')
+	r = [dict((cur.description[i][0], value)
+			  for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'medicos' : r})
--- API/DrBaratoAPI.py
+++ API/DrBaratoAPI.py
@@ -1,29 +1,13 @@
 from flask import Flask, jsonify, request, json
 from flaskext.mysql import MySQL
+import DAO
 
 app = Flask(__name__)
-mysql = MySQL()
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'drbarato'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
-mysql.init_app(app)
-
-@app.route('/medicos', methods=['POST'])
+@app.route('/medicos', methods=['POST','GET'])
 def medicos():
 	jss = request.get_json()
-	# data = json.loads(jss)
-	return getMedicos(jss["city"],jss["speciality"],jss["state"])
-
-def getMedicos(cidade,especialidade,estado):
-	cur = mysql.connect().cursor()
-	cur.execute("select * from drbarato.medico")
-	r = [dict((cur.description[i][0], value)
-			  for i, value in enumerate(row)) for row in cur.fetchall()]
-	return jsonify({'medicos' : r})
+	return DAO.getMedicos(jss["city"],jss["speciality"],jss["state"])
 
 if __name__ == '__main__':
 	app.run()
